0ddf6e212c71f354f0bcc91e5cc860ccf1986eb2
Rafael William da Silva <rafael.silva@inatel.br>
2017-10-26 16:16:09 -0200

    Correcao do servico de notificacao

--- www/js/gerenciadorService.js
+++ www/js/gerenciadorService.js
@@ -6,6 +6,42 @@ var GerenciadorService = {
         var lancamento = new Lancamento(data, descricao, valor, tipo);
         lancamento.id = this.lancamentos.length;
         this.lancamentos.push(lancamento);
+
+        /* Enviar Notificação após novo Lançamento
+        Exemplo de Utilização do FIREBASE CLOUD MESSAGING */
+        var keyServer = "AAAADdZ7zYc:APA91bEXgbcn7J2nNtinlcdOjK9_25jyi93jU6O6PX7l-q2XIiPlSxhiQNiZsV2JPf5zbFmGh86IEeOVCmqEgYhm1mpH-ZXSwYUhkCo8aSMKG5FBNYFFAW8Qe3BtS-GFS_vwTHKakQSA";
+
+        var notificationMessage = JSON.stringify({
+                "data": {
+                    "data": lancamento.data,
+                    "descrição": lancamento.descricao,
+                    "valor": lancamento.valor,
+                    "tipo": lancamento.tipo,
+                },
+                "priority": "high",
+                "notification": {
+                    "body": "Detalhes",
+                    "title": "Novo Lançamento"
+                },
+                "to": "/topics/lancamentos"
+            })
+
+        $.ajax({
+            type: 'POST',
+            url: 'https://fcm.googleapis.com/fcm/send',
+            headers: {
+                Authorization: 'key=' + keyServer,
+            },
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            data: notificationMessage,
+            success: function (response) {
+                //alert(JSON.stringify(response));
+            }, error: function (error) {
+                console.log('Erro ao enviar notificação');
+                //alert(JSON.stringify(error));
+            }
+        });
     },
 
     listar: function () {
